0b50fb7f2b65b61edacc3beab2a471e02eeca92c
ioav-livneh <ioavlivneh@gmail.com>
2023-12-06 11:19:24 -0600

    exercises 3 and 4

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -5,7 +5,20 @@ import BlogHero from "@/components/BlogHero";
 import styles from "./postSlug.module.css";
 
 import { loadBlogPost } from "../../helpers/file-helpers";
+
 import { MDXRemote } from "next-mdx-remote/rsc";
+
+import { BLOG_TITLE } from "@/constants";
+import CodeSnippet from "@/components/CodeSnippet/CodeSnippet";
+
+export async function generateMetadata({ params }) {
+  const { frontmatter } = await loadBlogPost(params.postSlug);
+
+  return {
+    title: `${frontmatter.title} • ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  };
+}
 
 async function BlogPost({ params }) {
   const { frontmatter, content } = await loadBlogPost(params.postSlug);
@@ -17,7 +30,7 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote source={content} components={{ pre: CodeSnippet }} />
       </div>
     </article>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,16 @@ import styles from "./homepage.module.css";
 
 import { getBlogPostList } from "../helpers/file-helpers";
 
+import { BLOG_TITLE } from "../constants";
+
+export const metadata = {
+  title: BLOG_TITLE,
+  description: "A wonderful blog about JavaScript",
+};
+
 async function Home() {
   const blogPosts = await getBlogPostList();
+
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>Latest Content:</h1>
